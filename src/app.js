8f48283cb5a070e6854b2a0972139ba1a761c533
BBRON-ASUS <aaronzhang587@gmail.com>
2022-04-21 12:07:30 -0700

    Added functions to display all contacts

---
+++ src/app.js
@@ -0,0 +1,77 @@
+// contact list, don't change this.
+const contactsList = [
+  {
+    name: "Togusa",
+    phone: "555 555-5555",
+    image: "togusa.png",
+    email: "togusa@secnine.com",
+    ID: "0"
+  },
+  {
+    name: "Tachikoma",
+    phone: "555 555-5555",
+    image: "tachikoma.png",
+    email: "tachikoma@secnine.com",
+    ID: "1"
+  },
+  {
+    name: "Saito",
+    phone: "555 555-5555",
+    image: "saito.png",
+    email: "saito@secnine.com",
+    ID: "2"
+  },
+  {
+    name: '"major" Motoko',
+    phone: "555 555-5555",
+    image: "motoko.png",
+    email: "motoko@secnine.com",
+    ID: "3"
+  },
+  {
+    name: "Ishikawa",
+    phone: "555 555-5555",
+    image: "ishikawa.png",
+    email: "ishikawa@secnine.com",
+    ID: "4"
+  },
+  {
+    name: "Batou",
+    phone: "555 555-5555",
+    image: "batou.png",
+    email: "batou@secnine.com",
+    ID: "5"
+  },
+  {
+    name: "Aramaki",
+    phone: "555 555-5555",
+    image: "aramaki.png",
+    email: "aramaki@secnine.com",
+    ID: "6"
+  },
+]
+
+// Your code goes here
+function load_all_contacts(contactsList) {
+  for (let i = 0; i < contactsList.length; i++) {
+    add_contact(contactsList[i]["name"])
+  }
+}
+
+function add_contact(contact_name) {
+  let contacts_area = document.getElementById('display_all_contacts');
+
+  let contact_card =
+  `<button onclick="load_single_contact()">
+    <img src="img/${contact_name.toLowerCase()}.png">
+    <p>${contact_name}</p>
+  </button>`
+
+  contacts_area.insertAdjacentHTML('beforeend', contact_card)
+}
+
+
+
+function load_single_contact() {
+  
+}
